939a7413539a965fb11d6c3cf8b05afc3c2b3e6e
cristisorinx <32891603+cristisorinx@users.noreply.github.com>
2018-02-25 00:23:06 +0200

    Update
    Am facut implementare pentru binar si hexa dar doar pentru numere pozitive si am incercat sa schimb valoare butoanelor si functia din onclick ca sa fac ca si la calculatorul din windows  (butonu cu sageata in sus )

--- Calculator/calcScript.js
+++ Calculator/calcScript.js
@@ -1,47 +1,47 @@
 		var box = document.getElementById('display');
 		var box2 = document.getElementById('display2');
+		var history = document.getElementById('textHistory');
+
 		var aux ;
 
+	//FUNCTION FOR DISPLAYING A CARACTER/NUMBER
 		var displayKey = function(x){
-			box.value = x;
+			box.value += x;
 
 			if(x == 'C'){
 				box.value = '';
 				box2.value = '';
 			}
-			//else if (x >='a' && 'z'<=x)
-				//box.value = 'NaN';
 		}
-
+	//FUNCTION FOR SAVING SOME VALUES ON DISPLAY2
 		var saveValue = function(x){
 			box2.value += box.value + x;
 			box.value = '';
 		}
-
-		var btnsub = function (x){
+	//FUNCTION FOR - BUTTON
+		var btnsub = function (){
 			if(box.value == '0' || box.value == '')
-			box.value = x;
+			box.value = '-';
 			else
-			saveValue(x);
+			saveValue('-');
 		}
-
-		var btnadd = function(x){
-			saveValue(x);
+	//FUNCTION FOR + BUTTON
+		var btnadd = function(){
+			saveValue('+');
 		}
-
+	//FUNCTION FOR / BUTTON	
 		var btndiv = function(x){
 			saveValue(x);
 		}
-
+	//FUNCTION FOR %(Mod) BUTTON
 		var btnrst = function(x){
-			saveValue(x)
+			saveValue(x);
 		}
-
-		var btnmul = function(){
-			box2.value += box.value + '*';
-			box.value = '';
+	//FUNCTION FOR * BUTTON
+		var btnmul = function(X){
+			saveValue(x);
 		}
-
+	//FUNCTION FOR = BUTTON
 		var btnequal = function(){
 			//find the operation we want to do
 			var str1 = box2.value;
@@ -59,7 +59,7 @@
 			box2.value = '';
 			}
 		}
-
+	//FUNCTION FOR CALCULATE X at Y
 		var btnequal2 = function(){
 			var str1 = box2.value;
 			var len = str1.length-1;
@@ -73,7 +73,7 @@
 			document.history.textHistory.value += str1 + y + ' = ' + result + '\n';
 			box2.value = '';
 		}
-
+	//FUNCTION FOR SQRT BUTTON
 		var btnsqrt = function(){
 			var x = box.value;
 			var result = Math.sqrt(x);
@@ -81,7 +81,7 @@
 			box.value = result;
 			document.history.textHistory.value += '√' + x + ' = ' + result + '\n';
 		}
-
+	//FUNCTION FOR SQUARE BUTTON
 		var btnsquare = function(){
 			document.history.textHistory.value += box.value;
 			var x = box.value;
@@ -90,11 +90,11 @@
 			box.value = result;
 			document.history.textHistory.value += '^2 = ' + result + '\n';
 		}
-
+	//FUNCTION FOR XatY BUTTON
 		var btnxaty = function(x){
 			saveValue(x);
 		}
-
+	//FUNCTION FOR FACTORIAL BUTTON
 		var btnfact = function(){
 			var fact=1;
 			var n = box.value;
@@ -111,7 +111,7 @@
 				document.history.textHistory.value += n + '! = ' + fact + '\n';
 			}
 		}
-
+	//FUNCTION FOR DELET BUTTON/BACKSPACE BUTTON
 		var btndel = function(){
 				var v = box.value;
 				var len = v.length-1;
@@ -119,7 +119,7 @@
 
 				box.value = newV;
 			}
-
+	//FUNCTION FOR SHOW AND HIDE THE HISTORY OF OPERATION DONE IN CALCULATOR
 		var popUpHistory =function(){
 
 		var currentName = document.getElementById('textHistory');
@@ -130,11 +130,12 @@
 			currentName.style.visibility = 'hidden' ;
 			
 		}
-
+	//FUNCTION FOR NEGATE BUTTON
 		var btnneg = function(){
 			var x = box.value;
 			var x2;
 			var stringHistory;
+
 			if(x > 0){
 				x2 = ~x + 1;
 				stringHistory = 'negate(' + x + ')';
@@ -151,3 +152,80 @@
 				document.history.textHistory.value += stringHistory + ' = ' + x + '\n';
 			}
 		}
+	//FUNCTION FOR HEXA CONVERTION FOR POZITIVE NUMBERS
+		var hexConvertion = function (){
+			var number = box.value;
+			var rem;
+			var valueArray = [];
+			var digits = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
+
+			while(number > 0){
+				rem = number % 16;
+				number = Math.floor(number / 16);
+				valueArray.push(rem);
+			}
+			var i = valueArray.length;
+			var j = 0;
+			var hexValue = [];
+
+			while (i > 0){
+				//document.history.textHistory.value +=  digits[valueArray.pop()];
+				hexValue[j] = digits[valueArray.pop()];
+				j++;
+				i--;
+			}
+			document.history.textHistory.value += hexValue.join("") + '\n';
+
+		}
+
+		var changeButton = function (){
+			var a = document.getElementById('hex');
+			a.value ='+';
+			document.getElementById('hex').onclick = btnadd;
+			document.getElementById('bin').value = '-';
+			document.getElementById('bin').onclick = btnsub;
+		}
+
+	//FUNCTION FOR BINARY CONVERTION
+	//----!!!! HALF DONE !!!-----NEGATIVE NUMBERS CONVERTION MISSING
+		var binaryConvertion = function(){
+			var number = box.value;
+			var rem ;
+			var bitsArray = [];
+			var negbits;
+			var i = 0;
+			
+			if(number > 0)
+			{
+				do{
+					rem = number % 2;
+					bitsArray.push(rem);
+					number = Math.floor(number / 2);
+				}while( number > 0);
+				bitsArray.push(number);
+
+				bitsArray.reverse();
+				document.history.textHistory.value += bitsArray.join("");
+				document.history.textHistory.value +='\n';
+			}
+		}
+				/*else if(number < 0){
+					number = ~number +1;
+					do{
+						rem = number % 2;
+						bitsArray.push(rem);
+						number = Math.floor(number / 2);
+					}while( number > 0);
+					bitsArray.push(number);
+					bitsArray.reverse();
+
+					document.history.textHistory.value +='bits value = ' + (number).toString(2) +'\n';
+				
+					negbits = bitsArray.join("");
+					
+					document.history.textHistory.value +='bits value = ' + negbits +'\n';
+					negbits = negbits + 1;
+
+					document.history.textHistory.value +='negbits value = ' + negbits +'\n';
+			}
+			document.history.textHistory.value +='bits value = ' + (number).toString(2) +'\n';	*/
